89ca616494dbd7823efcd588a254da67d803f0e5
noctarius <me@noctarius.com>
2014-08-11 13:24:25 +0200

    Onheap slab benchmark

--- src/main/java/com/hazelcast/onheapslab/OnheapSlabBenchmark.java
+++ src/main/java/com/hazelcast/onheapslab/OnheapSlabBenchmark.java
@@ -16,36 +16,137 @@
 
 package com.hazelcast.onheapslab;
 
+import com.hazelcast.nio.BufferObjectDataOutput;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializable;
+import com.hazelcast.nio.serialization.SerializationService;
+import com.hazelcast.nio.serialization.SerializationServiceBuilder;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @State(Scope.Benchmark)
+@Fork(jvmArgsPrepend = {"-Xmx10G"})
 @OperationsPerInvocation(OnheapSlabBenchmark.OPERATIONS_PER_INVOCATION)
 public class OnheapSlabBenchmark {
 
-    public static final int OPERATIONS_PER_INVOCATION = 1000000;
+    public static final int OPERATIONS_PER_INVOCATION = 10000;
 
-    @Benchmark
-    public Object[] testHashMap() throws Exception {
-        return testInternal(new HashMap<String, Entity>());
+    //27487790
+
+    private final Random random = new Random();
+
+    private SerializationService serializationService;
+
+    @Param(value = {"JDK", "SLAB", "OFFHEAP"})
+    private String type;
+
+    private Map<Integer, byte[]> map;
+
+    @Setup(Level.Trial)
+    public void benchmarkSetup() {
+        serializationService = new SerializationServiceBuilder().setAllowUnsafe(true).setUseNativeByteOrder(true).build();
+        map = createMap();
+    }
+
+    @TearDown(Level.Trial)
+    public void benchmarkTeardown() {
+        if (map instanceof SlapMap) {
+            ((SlapMap) map).destroy();
+        }
+    }
+
+    @TearDown(Level.Iteration)
+    public void teardown() {
+        map.clear();
+    }
+
+    public Map<Integer, byte[]> createMap() {
+        Map<Integer, byte[]> map;
+        if ("SLAB".equals(type)) {
+            map = new SlapMap<Integer>(false);
+        } else if ("OFFHEAP".equals(type)) {
+            map = new SlapMap<Integer>(true);
+        } else if ("JDK".equals(type)) {
+            map = new HashMap<Integer, byte[]>();
+        } else {
+            throw new RuntimeException("Unknown map type");
+        }
+        return map;
     }
 
     @Benchmark
-    public Object[] testOnheapSlabMap() throws Exception {
-        return testInternal(new ObheapSlabMap<String, Entity>());
+    public long testInternal() {
+        long h = 0;
+        for (int i = 0; i < 10000; i++) {
+            byte[] entity = buildEntity();
+            map.put(i, entity);
+            byte[] e = map.get(i);
+            h += e.hashCode();
+        }
+        return h;
     }
 
-    private Entity[] testInternal(Map<String, Entity> map) {
-        return null;
+    public static void main(String[] args) {
+        OnheapSlabBenchmark benchmark = new OnheapSlabBenchmark();
+        benchmark.type = "SLAB";
+        benchmark.benchmarkSetup();
+        for (int i = 0; i < 100; i++) {
+            benchmark.testInternal();
+            benchmark.teardown();
+        }
+        benchmark.benchmarkTeardown();
     }
 
-    private static final class Entity {
+    private byte[] buildEntity() {
+        try {
+            Entity entity = new Entity();
+            entity.foo = new byte[1000 + random.nextInt(1000)];
+            BufferObjectDataOutput objectDataOutput = serializationService.createObjectDataOutput(2100);
+            objectDataOutput.writeObject(entity);
+            byte[] buffer = objectDataOutput.getBuffer();
+            byte[] temp = new byte[objectDataOutput.position()];
+            System.arraycopy(buffer, 0, temp, 0, objectDataOutput.position());
+            return temp;
 
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final class Entity
+            implements DataSerializable {
+
+        private byte[] foo;
+
+        @Override
+        public void writeData(ObjectDataOutput objectDataOutput)
+                throws IOException {
+
+            objectDataOutput.writeInt(foo.length);
+            objectDataOutput.write(foo);
+        }
+
+        @Override
+        public void readData(ObjectDataInput objectDataInput)
+                throws IOException {
+
+            int length = objectDataInput.readInt();
+            foo = new byte[length];
+            objectDataInput.readFully(foo);
+        }
     }
 
 }
